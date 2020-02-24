ac640bd3320c976ec96aa5271ccb550571916490
Sven Lukas <Sven.Lukas@googlemail.com>
2020-02-24 19:23:48 +0100

    Minor fix: removed 'const' at getModule() according to improved library module load system.

--- src/main/logbook4esl/Module.cpp
+++ src/main/logbook4esl/Module.cpp
@@ -134,7 +134,7 @@ Module::Module()
 
 } /* anonymous namespace */
 
-const esl::module::Module& getModule() {
+esl::module::Module& getModule() {
 	if(isInitialized == false) {
 		/* ***************** *
 		 * initialize module *
