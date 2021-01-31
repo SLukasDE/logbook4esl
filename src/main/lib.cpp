7553712415942a5711ba4fd4e3fa4798f4c08d7d
Sven Lukas <Sven.Lukas@googlemail.com>
2021-01-31 20:39:43 +0100

    Minor changes in lib.cpp and license preamble

--- src/main/lib.cpp
+++ src/main/lib.cpp
@@ -1,6 +1,11 @@
-#include <esl/module/Library.h>
 #include <logbook4esl/Module.h>
 
+#include <esl/module/Library.h>
+#include <esl/Module.h>
+
 extern "C" esl::module::Module* esl__module__library__getModule(const std::string& moduleName) {
+	if(moduleName == "esl") {
+		return &esl::getModule();
+	}
 	return &logbook4esl::getModule();
 }
